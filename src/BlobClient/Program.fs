63e3e80ec1c8cc861774bccd224a21fd5cb52d8f
Dominik Raniszewski <dominik.raniszewski@gmail.com>
2020-06-25 11:44:20 +1000

    Fixed the sample client

--- src/BlobClient/Program.fs
+++ src/BlobClient/Program.fs
@@ -3,6 +3,7 @@ module BlobClient
 open FSharp.Azure.Blob
 open System.IO
 open System
+open Azure.Storage.Blobs.Models
 
 let readFile containerName blob =
     let connString = "UseDevelopmentStorage=true"
@@ -10,10 +11,9 @@ let readFile containerName blob =
                     |> Blob.fromConnectionString 
                     |> Blob.container containerName
                     |> Blob.readBlob blob
-                    |> Blob.execAsync
-
-    match result with
-    | BlobDownloadInfoAsync info -> info
+                    |> Blob.execAsync<BlobDownloadInfo>
+    result
+    
 
 
 let uploadFile containerName file =
@@ -24,11 +24,10 @@ let uploadFile containerName file =
                     |> Blob.createOrUpdate "test.json" file
                     |> Blob.overwriteBlob true
                     |> Blob.createContainer true
-                    |> Blob.execAsync
+                    |> Blob.execAsync<BlobContentInfo>
 
+    result
 
-    match result with
-    | BlobContentInfoAsync info -> info
 
 [<EntryPoint>]
 let main _ =
@@ -36,14 +35,14 @@ let main _ =
     use file = File.OpenRead "test.json"
 
     async {
-        let! blobInfo = uploadFile "container" file
+        let! blobInfo = uploadFile "invoice" file
         blobInfo.Value.ContentHash
         |> BitConverter.ToString
         |> Console.WriteLine
     } |> Async.RunSynchronously
 
     async {
-        let! blobInfo = readFile "container" "test.json"
+        let! blobInfo = readFile "invoice" "test.json"
         use sw = new StreamReader(blobInfo.Value.Content)
         sw.ReadToEnd() |> Console.WriteLine
         
